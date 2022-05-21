6d8c15d24374253b3ce730a8ada54a9f9b51fc96
Mychael Dourado <mychaeldourado23@gmail.com>
2022-05-21 14:21:25 -0300

    Adicionando manipulação do evento de touch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
 const btnMenu = document.querySelector('#btn--menu')
 
-const showMenuMobile = () => {
+const showMenuMobile = event => {
+
+  event.type === 'touchstart' ? e.preventDefault() : ''
+
   const navbar = document.querySelector('.navbar')
 
   navbar.classList.toggle('mobile')
+
 }
 
+btnMenu.addEventListener('click', showMenuMobile)
 btnMenu.addEventListener('touchstart', showMenuMobile)
